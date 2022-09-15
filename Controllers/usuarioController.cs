2931ed19d3669ebb40cc6abb13973f6dd7b9cae7
Fernanda  de Almeida Nery <fernanda.a.nery23@gmail.com>
2022-09-14 21:16:00 -0300

    first commit

---
+++ Controllers/usuarioController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_API.Controllers
+{
+    
+    [Route("[Controller]")]
+    public class usuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortDateString()
+            };
+
+            return Ok (obj);
+        }
+
+        //Metodo com parametro
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar (string nome) 
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo ! ";
+            return Ok (new {mensagem});
+        }
+        
+    }
+}
